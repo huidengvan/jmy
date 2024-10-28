d3e301c4819ab989c5bd8417d83f0acc0de732ce
Yuan Hang <zuokun@gmail.com>
2024-10-28 09:05:09 -0700

    hd_download baseline for mac

---
+++ jmy-vlist.sh
@@ -0,0 +1,9 @@
+./hd_download < ./401.in > 401.out
+./hd_download < ./402.in > 402.out
+./hd_download < ./403.in > 403.out
+
+./hd_download < ./404.in > 404.out
+./hd_download < ./405.in > 405.out
+./hd_download < ./406.in > 406.out
+./hd_download < ./407.in > 407.out
+./hd_download < ./408.in > 408.out
